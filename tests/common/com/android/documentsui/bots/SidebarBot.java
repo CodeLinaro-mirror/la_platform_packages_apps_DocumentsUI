df2c58545829ce67eb8bc3d9a5283db6689bdab3
Ben Reich <benreich@google.com>
2025-03-24 17:51:10 +1100

    Fix crash when pasting on a root in the sidebar
    When the right click menu "Paste into folder" is clicked from a root
    in the sidebar, it causes a crash. This is due to the DocumentStack
    being artificially created and the root and stack containing the same
    information. When the paste happens a new DocumentInfo is attempted
    to be merged with the existing one and these are identical which is
    what causes the crash.
    
    Fortunately this is not a truly necessary operation and is causing
    an extra copy of the DocumentStack to be created. Let's optimise and
    avoid recreating if we don't truly have to. Furthermore this adds a
    test that fails without the patch and succeeds with the patch.
    
    Fix: 403641008
    Test: atest com.android.documentsui.SidebarUiTest
    Flag: EXEMPT bug fix
    
    Change-Id: If4e49b4dd1e118b9e3a1fdaa590e663f41b05941

--- tests/common/com/android/documentsui/bots/SidebarBot.java
+++ tests/common/com/android/documentsui/bots/SidebarBot.java
@@ -21,8 +21,12 @@ import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.app.UiAutomation;
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.test.uiautomator.UiDevice;
@@ -47,9 +51,11 @@ public class SidebarBot extends Bots.BaseBot {
     private static final String TAG = "RootsListBot";
 
     private final String mRootListId;
+    private final UiAutomation mAutomation;
 
-    public SidebarBot(UiDevice device, Context context, int timeout) {
+    public SidebarBot(UiDevice device, UiAutomation automation, Context context, int timeout) {
         super(device, context, timeout);
+        mAutomation = automation;
         mRootListId = mTargetPackage + ":id/roots_list";
     }
 
@@ -134,5 +140,36 @@ public class SidebarBot extends Bots.BaseBot {
 
     public void assertHasFocus() {
         assertHasFocus(mRootListId);
+    }
+
+    /** Right clicks a root with `label`. */
+    public void rightClickRoot(String label) throws UiObjectNotFoundException {
+        Rect point = findRoot(label).getVisibleBounds();
+
+        // The RootsFragment listens to right clicks in the GenericMotionListener. This is to allow
+        // for a left and right click to be used interchangeably. This means to mock this behaviour,
+        // 4 input events needs to be synthesized. A down, button press, button release and an up.
+        MotionEvent motionDown =
+                getTestRightClickMotionEvent(
+                        MotionEvent.ACTION_DOWN, point.centerX(), point.centerY());
+        mAutomation.injectInputEvent(motionDown, true);
+        SystemClock.sleep(25);
+
+        MotionEvent motionButtonPress =
+                getTestRightClickMotionEvent(
+                        MotionEvent.ACTION_BUTTON_PRESS, point.centerX(), point.centerY());
+        mAutomation.injectInputEvent(motionButtonPress, true);
+        SystemClock.sleep(25);
+
+        MotionEvent motionButtonRelease =
+                getTestRightClickMotionEvent(
+                        MotionEvent.ACTION_BUTTON_RELEASE, point.centerX(), point.centerY());
+        mAutomation.injectInputEvent(motionButtonRelease, true);
+        SystemClock.sleep(25);
+
+        MotionEvent motionUp =
+                getTestRightClickMotionEvent(
+                        MotionEvent.ACTION_UP, point.centerX(), point.centerY());
+        mAutomation.injectInputEvent(motionUp, true);
     }
 }
