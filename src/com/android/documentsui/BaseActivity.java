8adbb890fda801c88cbd9d70609a12c1842ceb35
Ben Reich <benreich@google.com>
2025-04-23 16:37:27 +1000

    Ensure nav rail respects the window insets as well
    When the nav rail layout is used, it currently doesn't get inset by the
    requested amount. This updates to ensure that all 3 layouts and all the
    containers are properly inset.
    
    Bug: 402604097
    Test: m DocumentsUIGoogle and inspect Pixel Tablet in portrait mode
    Flag: com.android.documentsui.flags.use_material3
    Change-Id: I2fc935b47aac7c3107e380acac16c6c942a0f1cf

--- src/com/android/documentsui/BaseActivity.java
+++ src/com/android/documentsui/BaseActivity.java
@@ -607,6 +607,12 @@ public abstract class BaseActivity
 
                         View rootsContainer = findViewById(R.id.container_roots);
                         rootsContainer.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
+
+                        View navRailContainer = findViewById(R.id.nav_rail_container_roots);
+                        if (navRailContainer != null) {
+                            navRailContainer.setPadding(
+                                    0, 0, 0, insets.getSystemWindowInsetBottom());
+                        }
                     }
 
                     return insets.consumeSystemWindowInsets();
