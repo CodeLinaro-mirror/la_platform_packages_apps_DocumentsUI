771dcc9e2bec14a7195a78a1d04ab41078c4195f
Ben Reich <benreich@google.com>
2025-06-15 23:23:49 -0700

    Revert "Finalize status message strings to align with specs"
    This reverts commit 8331eca37a70a170343b2edbb3a889347e53bad6.
    
    Reason for revert: Causing lots of test failures super close to
    mainline train boarding.
    
    Bug: 407674931
    Change-Id: Ia239d0c4ee72ea42b842f7968a2ae54ff9dbdfd3

--- tests/functional/com/android/documentsui/JobPanelUiTest.kt
+++ tests/functional/com/android/documentsui/JobPanelUiTest.kt
@@ -183,7 +183,7 @@ class JobPanelUiTest : ActivityTestJunit4<FilesActivity>() {
             ))
         onView(withChild(withText(progress2.msg)))
             .check(selectedDescendantsMatch(
-                withText(R.string.move_failed),
+                withText(R.string.job_progress_item_failed),
                 isDisplayed()
             ))
             .check(selectedDescendantsMatch(
@@ -363,7 +363,7 @@ class JobPanelUiTest : ActivityTestJunit4<FilesActivity>() {
         onView(withId(R.id.job_progress_toolbar_badge)).check(matches(isDisplayed()))
 
         openPanel()
-        onView(withText(R.string.copy_failed)).perform(click())
+        onView(withText(R.string.job_progress_item_failed)).perform(click())
         onView(allOf(withId(R.id.job_progress_item_dismiss), isDisplayed())).perform(click())
         Espresso.pressBack()
         onView(withId(R.id.job_progress_toolbar_indicator)).check(matches(withProgress(40)))
