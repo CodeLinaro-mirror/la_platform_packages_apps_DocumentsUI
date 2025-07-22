2627b456a333693d4fd07c4a7140ba7c403d06f3
Alexander Bolodurin <alexbn@google.com>
2025-07-22 02:10:36 +0000

    [docsui] More robust root selection in sidebar in tests
    1. Skip clicking on a root in the sidebar if the target root is already
       selected.
    
       UiObject.click() waits for a TYPE_WINDOW_CONTENT_CHANGED or
       TYPE_VIEW_SELECTED accessibility event, and will return false if a
       timeout (3s by default) is reached without an event happening. If the
       target root is already selected, clicking it doesn't appear to
       generate these events, failing the test.
    
    2. Since we are only clicking if the root isn't already selected,
       the drawer should be closed automatically, so stop closing it
       manually.
    
    3. Wait for the title to change after clicking.
    
    4. Make checks in assertWindowTitle more specific.
    
       Only check header_title for pre-m3 recents root, because checking
       header_title substring for other roots is not a reliable indicator
       that the root is selected, as the title will contain the root's name
       even if a subdirectory is selected.
    
    This also rolls back ag/34396707, which isn't needed anymore.
    
    Bug: 428680975
    Test: atest DocumentsUIGoogleTests:com.android.documentsui.CancelFromNotificationUiTest
    Flag: EXEMPT test change
    Change-Id: I6b780d674ed8f6c293dfbbb87c50e74bfbaf4ca7

--- tests/common/com/android/documentsui/bots/UiBot.java
+++ tests/common/com/android/documentsui/bots/UiBot.java
@@ -25,7 +25,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static com.android.documentsui.util.FlagUtils.isUseMaterial3FlagEnabled;
@@ -115,14 +114,14 @@ public class UiBot extends Bots.BaseBot {
 
     public void assertWindowTitle(String expected) {
         mDevice.waitForIdle(mTimeout);
-        if (isUseMaterial3FlagEnabled()) {
-            onView(TOOLBAR).check(matches(withToolbarTitle(is(expected))));
+        if (!isUseMaterial3FlagEnabled() && expected.equals("Recent")) {
+            // When m3 is off and `show_search_bar` is on, the toolbar on the "recent files" screen
+            // on phones contains a search bar instead of a title, so let's check the `header_title`
+            // instead. NOTE: header_title doesn't exist in m3.
+            onView(withId(R.id.header_title)).check(matches(withText("Recent files")));
             return;
         }
-        // For pre-m3, the title might not be visible in the toolbar, when `show_search_bar` is
-        // true, because instead it might display the search bar for narrow screens (phones).
-        // Let's check the `header_title` instead. NOTE: header_title doesn't exist in m3.
-        onView(withId(R.id.header_title)).check(matches(withSubstring(expected)));
+        onView(TOOLBAR).check(matches(withToolbarTitle(is(expected))));
     }
 
     /**
