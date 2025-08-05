91c57c6569a4b1f6ab99adc748a4108f34d51d64
Olivia Tardjono <oliviatardjono@google.com>
2025-08-05 04:02:52 +0000

    [DocsUI Home screen] Add SidebarEntryItemInfo and ShortcutInfo.
    See go/homescreen-files-docsui-dd.
    
    To represent shortcuts, a similar approach to the
    current implementation with `RootInfo` will be used.
    Add an overarching interface `SidebarEntryItemInfo` for shared methods
    and characteristics between a regular documents provider `RootInfo`
    and the new `ShortcutInfo` class.
    
    Bug: 438374323
    Test: atest DocumentsUIGoogleTests:com.android.documentsui.base.ShortcutInfoTest
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I507b2ea70651b90d77b64f609f4a222ef32982ff

---
+++ src/com/android/documentsui/base/SidebarEntryItemInfo.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.documentsui.base
+
+import android.net.Uri
+
+/**
+ * The interface containing common attributes and methods between a shortcut and a root that is
+ * required to visually display the sidebar entry items.
+ */
+interface SidebarEntryItemInfo {
+    val root: RootInfo
+    val documentId: String?
+    val uri: Uri?
+    val title: String?
+}
