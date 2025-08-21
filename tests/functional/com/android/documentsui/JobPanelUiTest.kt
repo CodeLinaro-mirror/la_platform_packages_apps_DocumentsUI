5ced75e69ef99b83a74f0d218be8595f37c7f813
Austin Tankiang <austinct@google.com>
2025-08-21 01:34:16 +0000

    Disable auto-dismiss of visual signals
    The current auto-dismiss timing is too short and makes it difficult to
    interact with the signals. Disable it for now so we can see how
    intrusive the signals are and as a stopgap measure for now.
    
    Bug: 439695733
    Test: atest 'DocumentsUIGoogleTests:com.android.documentsui.JobPanelViewModelTest'
    Test: atest 'DocumentsUIGoogleTests:com.android.documentsui.JobPanelUiTest'
    Flag: com.android.documentsui.flags.visual_signals_ro
    Change-Id: I96400e386b29acbd504382e4a49e3d4d7b454a47

--- tests/functional/com/android/documentsui/JobPanelUiTest.kt
+++ tests/functional/com/android/documentsui/JobPanelUiTest.kt
@@ -52,10 +52,8 @@ import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
-import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Assume.assumeTrue
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -108,18 +106,6 @@ class JobPanelUiTest : ActivityTestJunit4<FilesActivity>() {
             .check(matches(isDisplayed()))
             .perform(click())
         onView(withId(R.id.job_progress_panel_title)).check(matches(isDisplayed()))
-    }
-
-    @Before
-    fun disableAutoDismiss() {
-        // The auto dismiss behaviour will cause some tests to flake depending on how fast the test
-        // is run, so just disable it.
-        JobPanelViewModel.disableAutoDismiss = true
-    }
-
-    @After
-    fun reenableAutoDismiss() {
-        JobPanelViewModel.disableAutoDismiss = false
     }
 
     @Test
