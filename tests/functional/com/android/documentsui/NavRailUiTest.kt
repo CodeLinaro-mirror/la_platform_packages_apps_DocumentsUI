6091eb5e7e1ebfc9f4f3272dd1a4f6d3da99585b
Wenbo Jie <wenbojie@google.com>
2025-09-10 04:51:40 +0000

    [DocsUI] Reformat the NavRailUiTest file
    Presubmit now checks the indent of the Kotlin file, to avoid mixing
    the formating diff with the actual logic change, this CL just reformat
    the Kotlin file before doing any actual logic change.
    
    Bug: 440965092
    Test: EXEMPT PURE_REFACTOR
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: If443ffb5d84129df56bcce051fade71a014272ba

--- tests/functional/com/android/documentsui/NavRailUiTest.kt
+++ tests/functional/com/android/documentsui/NavRailUiTest.kt
@@ -43,69 +43,71 @@ import org.junit.runner.RunWith
 @EnableFlags(FLAG_USE_MATERIAL3)
 @RunWith(AndroidJUnit4::class)
 class NavRailUiTest : ActivityTestJunit4<FilesActivity>() {
-  @get:Rule
-  val setFlags = OverrideFlagsRule()
+    @get:Rule val setFlags = OverrideFlagsRule()
 
-  companion object {
-    private const val MEDIUM_WINDOW_WIDTH = 700
-    private const val MEDIUM_WINDOW_HEIGHT = 900
-  }
-
-  /** Override the base method to launch activity in a specified window size. */
-  override fun launchActivity() {
-    // check assumption before launching the activity.
-    assumeMinWindowSizeAndFreeFormWindowFeature()
-
-    val intent = Intent(context, FilesActivity::class.java)
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-    if (this.initialRoot != null) {
-      intent.setAction(Intent.ACTION_VIEW)
-      intent.setDataAndType(this.initialRoot!!.uri, DocumentsContract.Root.MIME_TYPE_ITEM)
+    companion object {
+        private const val MEDIUM_WINDOW_WIDTH = 700
+        private const val MEDIUM_WINDOW_HEIGHT = 900
     }
-    val displayMetrics = Resources.getSystem().displayMetrics
-    val options = ActivityOptions.makeBasic()
-    options.launchWindowingMode = WINDOWING_MODE_FREEFORM
-    options.setLaunchBounds(
-      Rect(
-        0,
-        0,
-        dpToPx(MEDIUM_WINDOW_WIDTH.toFloat(), displayMetrics).roundToInt(),
-        dpToPx(MEDIUM_WINDOW_HEIGHT.toFloat(), displayMetrics).roundToInt(),
-      )
-    )
-    options.setLaunchDisplayId(Display.DEFAULT_DISPLAY)
-    mActivityScenario = ActivityScenario.launch(intent, options.toBundle())
-  }
 
-  /** Making sure the test device meets the minimum window size and have freeform window feature. */
-  fun assumeMinWindowSizeAndFreeFormWindowFeature() {
-    assumeTrue(
-      "Skipping test: test device doesn't support FreeForm window.",
-      context!!.getPackageManager().hasSystemFeature(FEATURE_FREEFORM_WINDOW_MANAGEMENT),
-    )
-    val displayMetrics = Resources.getSystem().displayMetrics
-    assumeTrue(
-      "Skipping test: test device window size is too small to support medium layout.",
-      pxToDp(displayMetrics.widthPixels.toFloat(), displayMetrics) >= MEDIUM_WINDOW_WIDTH &&
-        pxToDp(displayMetrics.heightPixels.toFloat(), displayMetrics) >= MEDIUM_WINDOW_HEIGHT,
-    )
-  }
+    /** Override the base method to launch activity in a specified window size. */
+    override fun launchActivity() {
+        // check assumption before launching the activity.
+        assumeMinWindowSizeAndFreeFormWindowFeature()
 
-  @DesktopTest(cujs = ["b/434065814", "b/434065378"])
-  @Test
-  fun testNavRailRootsNavigation() {
-    bots.main.assertWindowTitle(StubProvider.ROOT_0_ID)
-    bots.roots.openNavRailRoot(StubProvider.ROOT_1_ID)
-    bots.main.assertWindowTitle(StubProvider.ROOT_1_ID)
-  }
+        val intent = Intent(context, FilesActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (this.initialRoot != null) {
+            intent.setAction(Intent.ACTION_VIEW)
+            intent.setDataAndType(this.initialRoot!!.uri, DocumentsContract.Root.MIME_TYPE_ITEM)
+        }
+        val displayMetrics = Resources.getSystem().displayMetrics
+        val options = ActivityOptions.makeBasic()
+        options.launchWindowingMode = WINDOWING_MODE_FREEFORM
+        options.setLaunchBounds(
+            Rect(
+                0,
+                0,
+                dpToPx(MEDIUM_WINDOW_WIDTH.toFloat(), displayMetrics).roundToInt(),
+                dpToPx(MEDIUM_WINDOW_HEIGHT.toFloat(), displayMetrics).roundToInt(),
+            )
+        )
+        options.setLaunchDisplayId(Display.DEFAULT_DISPLAY)
+        mActivityScenario = ActivityScenario.launch(intent, options.toBundle())
+    }
 
-  @Test
-  fun testNavRailDrawerRootsNavigation() {
-    bots.main.assertWindowTitle(StubProvider.ROOT_0_ID)
-    bots.roots.openDrawerFromNavRail()
-    // The following openRoot will open root from the drawer instead of the navigation rail because
-    // the drawer is open explicitly above.
-    bots.roots.openRoot(StubProvider.ROOT_1_ID)
-    bots.main.assertWindowTitle(StubProvider.ROOT_1_ID)
-  }
+    /**
+     * Making sure the test device meets the minimum window size and have freeform window feature.
+     */
+    fun assumeMinWindowSizeAndFreeFormWindowFeature() {
+        assumeTrue(
+            "Skipping test: test device doesn't support FreeForm window.",
+            context!!.getPackageManager().hasSystemFeature(FEATURE_FREEFORM_WINDOW_MANAGEMENT),
+        )
+        val displayMetrics = Resources.getSystem().displayMetrics
+        assumeTrue(
+            "Skipping test: test device window size is too small to support medium layout.",
+            pxToDp(displayMetrics.widthPixels.toFloat(), displayMetrics) >= MEDIUM_WINDOW_WIDTH &&
+                pxToDp(displayMetrics.heightPixels.toFloat(), displayMetrics) >=
+                    MEDIUM_WINDOW_HEIGHT,
+        )
+    }
+
+    @DesktopTest(cujs = ["b/434065814", "b/434065378"])
+    @Test
+    fun testNavRailRootsNavigation() {
+        bots.main.assertWindowTitle(StubProvider.ROOT_0_ID)
+        bots.roots.openNavRailRoot(StubProvider.ROOT_1_ID)
+        bots.main.assertWindowTitle(StubProvider.ROOT_1_ID)
+    }
+
+    @Test
+    fun testNavRailDrawerRootsNavigation() {
+        bots.main.assertWindowTitle(StubProvider.ROOT_0_ID)
+        bots.roots.openDrawerFromNavRail()
+        // The following openRoot will open root from the drawer instead of the navigation rail
+        // because the drawer is open explicitly above.
+        bots.roots.openRoot(StubProvider.ROOT_1_ID)
+        bots.main.assertWindowTitle(StubProvider.ROOT_1_ID)
+    }
 }
