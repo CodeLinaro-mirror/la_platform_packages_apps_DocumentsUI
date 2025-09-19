f1a41ad27b1a3c18f8282a594bf95d22d05a725b
Olivia Tardjono <oliviatardjono@google.com>
2025-09-19 05:53:00 +0000

    [DocsUI reformat] Reformat RecyclerRootsAdapter and its test.
    Bug: 438374404
    Test: EXEMPT PURE_REFACTOR
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I21bdc4e48f31dccfd8eee47aef40e4de57a56836

--- tests/unit/com/android/documentsui/sidebar/RecyclerRootsAdapterTest.kt
+++ tests/unit/com/android/documentsui/sidebar/RecyclerRootsAdapterTest.kt
@@ -47,180 +47,182 @@ import org.mockito.MockitoAnnotations
 @SmallTest
 class RecyclerRootsAdapterTest {
 
-  @Mock private lateinit var activity: BaseActivity
-  @Mock private lateinit var dragListener: OnDragListener
+    @Mock private lateinit var activity: BaseActivity
+    @Mock private lateinit var dragListener: OnDragListener
 
-  private lateinit var parent: RecyclerView
-  private lateinit var adapter: RecyclerRootsAdapter
-  private lateinit var items: MutableList<Item>
+    private lateinit var parent: RecyclerView
+    private lateinit var adapter: RecyclerRootsAdapter
+    private lateinit var items: MutableList<Item>
 
-  @get:Rule val expect = Expect.create()
+    @get:Rule val expect = Expect.create()
 
-  @Before
-  fun setup() {
-    MockitoAnnotations.openMocks(this)
-    val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
-    parent = RecyclerView(context)
-    parent.layoutManager = LinearLayoutManager(context)
-    items = mutableListOf()
-    adapter = RecyclerRootsAdapter(activity, items, dragListener)
-  }
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        parent = RecyclerView(context)
+        parent.layoutManager = LinearLayoutManager(context)
+        items = mutableListOf()
+        adapter = RecyclerRootsAdapter(activity, items, dragListener)
+    }
 
-  @Test
-  fun testOnBindViewHolder() {
-    val mockItem = mock(RootItem::class.java)
-    items.add(mockItem)
-    val viewHolder = adapter.onCreateViewHolder(parent, 0)
+    @Test
+    fun testOnBindViewHolder() {
+        val mockItem = mock(RootItem::class.java)
+        items.add(mockItem)
+        val viewHolder = adapter.onCreateViewHolder(parent, 0)
 
-    adapter.onBindViewHolder(viewHolder, 0)
+        adapter.onBindViewHolder(viewHolder, 0)
 
-    verify(mockItem).bindView(viewHolder.itemView)
-    expect.that(viewHolder.itemView.getTag(R.id.item_position_tag)).isEqualTo(0)
-  }
+        verify(mockItem).bindView(viewHolder.itemView)
+        expect.that(viewHolder.itemView.getTag(R.id.item_position_tag)).isEqualTo(0)
+    }
 
-  @Test
-  fun testOnBindViewHolder_withSpaceItem() {
-    val mockItem = SpacerItem()
-    items.add(mockItem)
-    val viewHolder = adapter.onCreateViewHolder(parent, 2)
+    @Test
+    fun testOnBindViewHolder_withSpaceItem() {
+        val mockItem = SpacerItem()
+        items.add(mockItem)
+        val viewHolder = adapter.onCreateViewHolder(parent, 2)
 
-    adapter.onBindViewHolder(viewHolder, 0)
+        adapter.onBindViewHolder(viewHolder, 0)
 
-    expect.that(viewHolder.itemView.isEnabled).isFalse()
-  }
+        expect.that(viewHolder.itemView.isEnabled).isFalse()
+    }
 
-  @Test
-  fun testOnBindViewHolder_withSelectedItem() {
-    val mockItem = mock(RootItem::class.java)
-    whenever(mockItem.isSelected).thenReturn(true)
-    items.add(mockItem)
-    val viewHolder = adapter.onCreateViewHolder(parent, 0)
+    @Test
+    fun testOnBindViewHolder_withSelectedItem() {
+        val mockItem = mock(RootItem::class.java)
+        whenever(mockItem.isSelected).thenReturn(true)
+        items.add(mockItem)
+        val viewHolder = adapter.onCreateViewHolder(parent, 0)
 
-    adapter.onBindViewHolder(viewHolder, 0)
+        adapter.onBindViewHolder(viewHolder, 0)
 
-    expect.that(viewHolder.itemView.isActivated).isTrue()
-    expect.that(viewHolder.itemView.isSelected).isTrue()
-  }
+        expect.that(viewHolder.itemView.isActivated).isTrue()
+        expect.that(viewHolder.itemView.isSelected).isTrue()
+    }
 
-  @Test
-  fun testOnBindViewHolder_withUnselectedItem() {
-    val mockItem = mock(RootItem::class.java)
-    whenever(mockItem.isSelected).thenReturn(false)
-    items.add(mockItem)
-    val viewHolder = adapter.onCreateViewHolder(parent, 0)
+    @Test
+    fun testOnBindViewHolder_withUnselectedItem() {
+        val mockItem = mock(RootItem::class.java)
+        whenever(mockItem.isSelected).thenReturn(false)
+        items.add(mockItem)
+        val viewHolder = adapter.onCreateViewHolder(parent, 0)
 
-    adapter.onBindViewHolder(viewHolder, 0)
+        adapter.onBindViewHolder(viewHolder, 0)
 
-    expect.that(viewHolder.itemView.isActivated).isFalse()
-    expect.that(viewHolder.itemView.isSelected).isFalse()
-  }
+        expect.that(viewHolder.itemView.isActivated).isFalse()
+        expect.that(viewHolder.itemView.isSelected).isFalse()
+    }
 
-  @Test
-  fun testOnBindViewHolder_onClick() {
-    // simply mock doesn't work, why?
-    val mockItem = spy(RootItem(mock(RootInfo::class.java), mock(ActionHandler::class.java), false))
+    @Test
+    fun testOnBindViewHolder_onClick() {
+        // simply mock doesn't work, why?
+        val mockItem =
+            spy(RootItem(mock(RootInfo::class.java), mock(ActionHandler::class.java), false))
 
-    adapter.onClick(mockItem)
+        adapter.onClick(mockItem)
 
-    verify(mockItem).open()
-  }
+        verify(mockItem).open()
+    }
 
-  @Test
-  fun testOnBindViewHolder_onLongClick() {
-    val mockItem = mock(RootItem::class.java)
-    whenever(activity.displayState).thenReturn(State().apply { action = State.ACTION_GET_CONTENT })
+    @Test
+    fun testOnBindViewHolder_onLongClick() {
+        val mockItem = mock(RootItem::class.java)
+        whenever(activity.displayState)
+            .thenReturn(State().apply { action = State.ACTION_GET_CONTENT })
 
-    adapter.onLongClick(mockItem)
+        adapter.onLongClick(mockItem)
 
-    verify(mockItem).showAppDetails()
-  }
+        verify(mockItem).showAppDetails()
+    }
 
-  @Test
-  fun testOnBindViewHolder_onTabKey() {
-    val features = mock(Features::class.java)
-    whenever(features.isSystemKeyboardNavigationEnabled()).thenReturn(true)
-    val injector =
-      Injector<ActionHandler>(
-        features,
-        mock(ActivityConfig::class.java),
-        null,
-        null,
-        null,
-        null,
-        null
-      )
-    whenever(activity.getInjector()).thenReturn(injector)
-    val event = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_TAB)
+    @Test
+    fun testOnBindViewHolder_onTabKey() {
+        val features = mock(Features::class.java)
+        whenever(features.isSystemKeyboardNavigationEnabled()).thenReturn(true)
+        val injector =
+            Injector<ActionHandler>(
+                features,
+                mock(ActivityConfig::class.java),
+                null,
+                null,
+                null,
+                null,
+                null,
+            )
+        whenever(activity.getInjector()).thenReturn(injector)
+        val event = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_TAB)
 
-    expect.that(adapter.onKey(KeyEvent.KEYCODE_TAB, event)).isFalse()
-  }
+        expect.that(adapter.onKey(KeyEvent.KEYCODE_TAB, event)).isFalse()
+    }
 
-  @Test
-  fun testOnBindViewHolder_onArrowKey() {
-    val leftArrowEvent = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT)
-    expect.that(adapter.onKey(KeyEvent.KEYCODE_DPAD_LEFT, leftArrowEvent)).isTrue()
+    @Test
+    fun testOnBindViewHolder_onArrowKey() {
+        val leftArrowEvent = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT)
+        expect.that(adapter.onKey(KeyEvent.KEYCODE_DPAD_LEFT, leftArrowEvent)).isTrue()
 
-    val rightArrowEvent = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT)
-    expect.that(adapter.onKey(KeyEvent.KEYCODE_DPAD_RIGHT, rightArrowEvent)).isTrue()
-  }
+        val rightArrowEvent = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT)
+        expect.that(adapter.onKey(KeyEvent.KEYCODE_DPAD_RIGHT, rightArrowEvent)).isTrue()
+    }
 
-  @Test
-  fun testGetItemViewType_withRootItem() {
-    items.add(mock(RootItem::class.java))
-    expect.that(adapter.getItemViewType(0)).isEqualTo(0)
-  }
+    @Test
+    fun testGetItemViewType_withRootItem() {
+        items.add(mock(RootItem::class.java))
+        expect.that(adapter.getItemViewType(0)).isEqualTo(0)
+    }
 
-  @Test
-  fun testGetItemViewType_withNavRailRootItem() {
-    items.add(mock(NavRailRootItem::class.java))
-    expect.that(adapter.getItemViewType(0)).isEqualTo(1)
-  }
+    @Test
+    fun testGetItemViewType_withNavRailRootItem() {
+        items.add(mock(NavRailRootItem::class.java))
+        expect.that(adapter.getItemViewType(0)).isEqualTo(1)
+    }
 
-  @Test
-  fun testGetItemViewType_withSpacerItem() {
-    items.add(SpacerItem())
-    expect.that(adapter.getItemViewType(0)).isEqualTo(2)
-  }
+    @Test
+    fun testGetItemViewType_withSpacerItem() {
+        items.add(SpacerItem())
+        expect.that(adapter.getItemViewType(0)).isEqualTo(2)
+    }
 
-  @Test
-  fun testGetItemViewType_withHeaderItem() {
-    items.add(HeaderItem("Test"))
-    expect.that(adapter.getItemViewType(0)).isEqualTo(3)
-  }
+    @Test
+    fun testGetItemViewType_withHeaderItem() {
+        items.add(HeaderItem("Test"))
+        expect.that(adapter.getItemViewType(0)).isEqualTo(3)
+    }
 
-  @Test
-  fun testSetItemSelected_updatesSelectedItemAndNotifies() {
-    val mockItem1 = mock(RootItem::class.java)
-    val mockItem2 = mock(RootItem::class.java)
-    val observer: AdapterDataObserver = mock(AdapterDataObserver::class.java)
-    adapter.registerAdapterDataObserver(observer)
-    items.add(mockItem1)
-    items.add(mockItem2)
+    @Test
+    fun testSetItemSelected_updatesSelectedItemAndNotifies() {
+        val mockItem1 = mock(RootItem::class.java)
+        val mockItem2 = mock(RootItem::class.java)
+        val observer: AdapterDataObserver = mock(AdapterDataObserver::class.java)
+        adapter.registerAdapterDataObserver(observer)
+        items.add(mockItem1)
+        items.add(mockItem2)
 
-    // Select item 0.
-    adapter.setItemSelected(0, true)
-    verify(mockItem1).isSelected = true
-    verify(observer).onItemRangeChanged(0, 1, null)
+        // Select item 0.
+        adapter.setItemSelected(0, true)
+        verify(mockItem1).isSelected = true
+        verify(observer).onItemRangeChanged(0, 1, null)
 
-    // Select item 1, item 0 should be unselected.
-    adapter.setItemSelected(1, true)
+        // Select item 1, item 0 should be unselected.
+        adapter.setItemSelected(1, true)
 
-    verify(mockItem1).isSelected = false
-    verify(mockItem2).isSelected = true
-    // Verify both item 1 and item 2 have changed.
-    verify(observer, times(2)).onItemRangeChanged(0, 1, null)
-    verify(observer).onItemRangeChanged(1, 1, null)
-  }
+        verify(mockItem1).isSelected = false
+        verify(mockItem2).isSelected = true
+        // Verify both item 1 and item 2 have changed.
+        verify(observer, times(2)).onItemRangeChanged(0, 1, null)
+        verify(observer).onItemRangeChanged(1, 1, null)
+    }
 
-  @Test
-  fun testGetItem() {
-    val mockItem1 = mock(RootItem::class.java)
-    val mockItem2 = mock(RootItem::class.java)
-    items.add(mockItem1)
-    items.add(mockItem2)
+    @Test
+    fun testGetItem() {
+        val mockItem1 = mock(RootItem::class.java)
+        val mockItem2 = mock(RootItem::class.java)
+        items.add(mockItem1)
+        items.add(mockItem2)
 
-    expect.that(adapter.itemCount).isEqualTo(2)
-    expect.that(adapter.getItem(0)).isEqualTo(mockItem1)
-    expect.that(adapter.getItem(1)).isEqualTo(mockItem2)
-  }
+        expect.that(adapter.itemCount).isEqualTo(2)
+        expect.that(adapter.getItem(0)).isEqualTo(mockItem1)
+        expect.that(adapter.getItem(1)).isEqualTo(mockItem2)
+    }
 }
