d28713b45888022eb4a5dea0a418af8e3ebb0225
Nigel Tao <nigeltao@google.com>
2025-10-16 17:38:24 +1100

    [DocsUI] add comment about Jumbo clips
    Bug: 435032907
    Test: EXEMPT DOCS_ONLY
    Flag: EXEMPT DOCS_ONLY
    Change-Id: Ia38291f36e38049819e9cf230ae4d040913f1fa2

--- src/com/android/documentsui/clipping/DocumentClipper.java
+++ src/com/android/documentsui/clipping/DocumentClipper.java
@@ -33,6 +33,10 @@ import java.util.function.Function;
 
 public interface DocumentClipper {
 
+    // A clip is the list of files passed to the FileOperationService when starting a file
+    // operation. A "jumbo clip" is when this list is too big to pass via IPC, so the URIs are
+    // written to a temp file on disk and this file is read by the FileOperationService instead.
+
     static final String OP_JUMBO_SELECTION_SIZE = "jumboSelection-size";
     static final String OP_JUMBO_SELECTION_TAG = "jumboSelection-tag";
 
