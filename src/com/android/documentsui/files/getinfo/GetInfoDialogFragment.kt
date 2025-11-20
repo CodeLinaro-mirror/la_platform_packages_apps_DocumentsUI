61feb1e0983e9edba4a1fc622aa921113bea7537
Ben Reich <benreich@google.com>
2025-11-20 23:35:51 +1100

    Add the directory count retrieval logic for the "Get info" dialog
    In the case where a directory has "Get info" invoked on it, there is no
    "Size" attribute. Instead the existing Inspector view shows the number
    of children items in the folder. This ports that logic over as well.
    
    This logic is inherently async, some DocumentsProviders can respond
    instantly (locally available) whilst others might take O(seconds) to
    respond. To cater for the latter, this CL refactors the existing code
    out into a ViewModel so that the updates can be done async and the
    Fragment properly responds to these (recalculating the layout as
    required).
    
    There's a few additional classes that are added to start moving those
    into their own package and structuring them into separate files.
    
    The addition of androidx.fragment_fragment-ktx was required to enable
    the lazy viewModel creation (i.e. by viewModels). This new library has
    an APK size increase of ~12 KB (before 10487947 bytes after 10500418
    when built using the brya-trunk_staging-eng build target).
    
    Bug: 458494432
    Flag: com.android.documentsui.flags.get_info_dialog
    Test: atest com.android.documentsui.files.getinfo.GetInfoViewModelTest
    Change-Id: Ic9780e789929cba49e28a376afa1b7a487cad38c

--- src/com/android/documentsui/files/getinfo/GetInfoDialogFragment.kt
+++ src/com/android/documentsui/files/getinfo/GetInfoDialogFragment.kt
@@ -17,38 +17,193 @@
 package com.android.documentsui.files.getinfo
 
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.VisibleForTesting
-import androidx.appcompat.app.AlertDialog
 import androidx.core.os.BundleCompat
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.android.documentsui.DocumentsApplication
+import com.android.documentsui.DocumentsUIDialogFragment
 import com.android.documentsui.R
 import com.android.documentsui.base.DocumentInfo
-import com.android.documentsui.base.Lookup
 import com.android.documentsui.base.Shared
-import com.android.documentsui.util.UnitUtils
+import com.android.documentsui.util.Material3Config
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlin.collections.get
+import kotlinx.coroutines.launch
 
 /**
  * A dialog that shows all the metadata aspects related to a file / folder. Is currently invoked
  * when the user clicks "Get info" in the 3-dot / context menus.
  */
-class GetInfoDialogFragment : DialogFragment() {
+class GetInfoDialogFragment : DocumentsUIDialogFragment() {
+
+    /** The Document that the dialog is retrieving information about. */
+    private lateinit var doc: DocumentInfo
+
+    /** Lazily initialized ViewModel. */
+    private val viewModel: GetInfoViewModel by viewModels {
+        val application = requireActivity().application
+        val fileTypeLookup = DocumentsApplication.getFileTypeLookup(requireContext())
+        GetInfoViewModel.Factory(application, doc, fileTypeLookup)
+    }
+
+    override fun onCreate(savedInstanceBundle: Bundle?) {
+        super.onCreate(savedInstanceBundle)
+        val arguments = arguments ?: savedInstanceBundle
+        doc =
+            arguments?.let {
+                BundleCompat.getParcelable(it, Shared.EXTRA_DOC, DocumentInfo::class.java)
+            }
+                ?: run {
+                    Log.e(TAG, "Document missing for Get info dialog, dismissing.")
+                    DocumentInfo() // Fallback to avoid lateinit crash if dismissed immediately.
+                }
+
+        if (doc.documentId == null) {
+            // Because we're using `lateinit` on the var (if it doesn't exist, that is bad) we need
+            // to double check the initial isn't the `DocumentInfo()` empty constructed version. If
+            // it is, just dismiss the dialog.
+            dismiss()
+        }
+    }
+
+    /** Creates the dialog UI. */
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = MaterialAlertDialogBuilder(requireContext())
+        val dialogView = LayoutInflater.from(builder.context).inflate(R.layout.get_info_m3, null)
+
+        return builder
+            .apply {
+                setTitle(Material3Config.getRes(R.string.peek_metadata_header_title))
+                setView(dialogView)
+                setPositiveButton(android.R.string.ok, null)
+            }
+            .create()
+    }
+
+    /** Start observing changes in the ViewModel. */
+    override fun onStart() {
+        super.onStart()
+        val contentLayout = dialog?.findViewById<LinearLayout>(R.id.content_layout)
+
+        contentLayout?.let {
+            lifecycleScope.launch {
+                viewModel.items.collect { items ->
+                    // Rebuild layout only when there are actually items in the list.
+                    if (items.isNotEmpty()) {
+                        rebuildLayout(it, items)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Given we know the number of items in the dialog will be small (<50) it's more performant to
+     * just blow the list away and rebuild than to smartly update it.
+     */
+    private fun rebuildLayout(container: LinearLayout, items: List<ListItem>) {
+        container.removeAllViews()
+
+        val inflater = LayoutInflater.from(container.context)
+        items.forEachIndexed { index, item ->
+            // Check if the item is the first in the section.
+            val isFirstInSection = if (index == 0) true else items[index - 1] is ListItem.Header
+
+            // Check if this is the last item in the section.
+            val isLastInSection =
+                if (index == items.size - 1) {
+                    true
+                } else {
+                    items[index + 1] is ListItem.Header
+                }
+
+            val view =
+                when (item) {
+                    is ListItem.Header -> createHeaderView(inflater, container, item)
+                    is ListItem.Info ->
+                        createInfoView(inflater, container, item, isFirstInSection, isLastInSection)
+                }
+            container.addView(view)
+        }
+    }
+
+    /**
+     * Creates a UI row out of ListItem.Header. This represents the title for the items to follow.
+     * e.g. "General info".
+     */
+    private fun createHeaderView(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        item: ListItem.Header,
+    ): View {
+        return inflater.inflate(R.layout.get_info_header_m3, parent, false).apply {
+            findViewById<TextView>(R.id.header_title).text = item.title
+        }
+    }
+
+    /**
+     * Creates a UI row out of ListItem.Info, e.g. "Type". Each row has 4 different possible states:
+     * - Single: Represents only a single item in the row (all 4 corners have a larger radius).
+     * - Top: When the list has >1 item (top 2 corners have a larger radius).
+     * - Middle: When the list has >2 items (no corners have larger radius).
+     * - Bottom: When the list has >1 item (bottom 2 corners have a larger radius). This is
+     *   calculated based on the number of items in each section and the styling applied to each
+     *   individual row.
+     */
+    private fun createInfoView(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        item: ListItem.Info,
+        isFirstInSection: Boolean,
+        isLastInSection: Boolean,
+    ): View {
+        val view = inflater.inflate(R.layout.get_info_item_m3, parent, false)
+
+        view.findViewById<TextView>(R.id.item_label).text = item.label
+        view.findViewById<TextView>(R.id.item_value).text = item.value
+
+        // Apply the correct background drawable based on the position of
+        // the item in the section. The border radius is different based on
+        // the position of the item.
+        val backgroundRes =
+            when {
+                isFirstInSection && isLastInSection -> R.drawable.get_info_item_single_m3
+                isFirstInSection -> R.drawable.get_info_item_top_m3
+                isLastInSection -> R.drawable.get_info_item_bottom_m3
+                else -> R.drawable.get_info_item_middle_m3
+            }
+        view.setBackgroundResource(backgroundRes)
+
+        // Apply the appropriate margins to the item according to the
+        // position in the list.
+        val params = view.layoutParams as ViewGroup.MarginLayoutParams
+        val gapBetweenItems = resources.getDimensionPixelSize(R.dimen.space_extra_small_1)
+        if (isFirstInSection && isLastInSection) {
+            // No padding as it's the only item in the list.
+            params.bottomMargin = 0
+        } else if (isFirstInSection) {
+            params.bottomMargin = gapBetweenItems
+        } else if (isLastInSection) {
+            params.bottomMargin = resources.getDimensionPixelSize(R.dimen.space_small_1)
+        } else {
+            params.bottomMargin = gapBetweenItems
+        }
+
+        view.layoutParams = params
+
+        return view
+    }
 
     companion object {
         private const val TAG = "GetInfoDialog"
-
-        private const val WIDTH = 396f
 
         /**
          * Creates the fragment and converts the supplied `doc` into an appropriate type to pass
@@ -62,7 +217,7 @@ class GetInfoDialogFragment : DialogFragment() {
             }
 
             if (fm.findFragmentByTag(TAG) != null) {
-                Log.w(TAG, "Skipping to get info dialog because it's already showing.")
+                Log.w(TAG, "Skip showing get info dialog because it's already showing.")
                 return
             }
 
@@ -70,188 +225,6 @@ class GetInfoDialogFragment : DialogFragment() {
             val args = Bundle().apply { putParcelable(Shared.EXTRA_DOC, doc) }
             dialog.arguments = args
             dialog.show(fm, TAG)
-        }
-
-        /**
-         * Generates the list of metadata related to the file, extracted from the `DocumentsInfo`
-         * that is passed on when the dialog is opened.
-         */
-        @VisibleForTesting
-        fun createDataList(
-            context: Context,
-            doc: DocumentInfo,
-            fileTypeLookup: Lookup<String, String>,
-        ): List<ListItem> {
-            val dataList = mutableListOf<ListItem>()
-
-            // Add the "General info" header title.
-            dataList.add(
-                ListItem.Header(context.getString(R.string.peek_metadata_general_info_title))
-            )
-
-            // Add the file display name.
-            dataList.add(
-                ListItem.Info(
-                    context.getString(R.string.sort_dimension_name),
-                    doc.displayName ?: "",
-                )
-            )
-
-            // Add the file type.
-            dataList.add(
-                ListItem.Info(
-                    context.getString(R.string.peek_metadata_type),
-                    fileTypeLookup.lookup(doc.mimeType)
-                        ?: context.getString(R.string.get_info_unknown_file_type),
-                )
-            )
-
-            // Add the file size (if one exists and it's not a directory).
-            if (doc.size >= 0 && !doc.isDirectory) {
-                dataList.add(
-                    ListItem.Info(
-                        context.getString(R.string.peek_metadata_size),
-                        DefaultInfoFormatter.formatFileSize(context, doc.size),
-                    )
-                )
-            }
-
-            // Add the file last modified date (if one exists).
-            if (doc.lastModified > 0) {
-                dataList.add(
-                    ListItem.Info(
-                        context.getString(R.string.peek_metadata_date_modified),
-                        DefaultInfoFormatter.formatDate(context, doc.lastModified),
-                    )
-                )
-            }
-
-            // Add the document summary (if one exists).
-            if (doc.isPartial && doc.summary != null) {
-                dataList.add(
-                    ListItem.Info(
-                        context.getString(R.string.sort_dimension_summary),
-                        doc.summary ?: "",
-                    )
-                )
-            }
-            return dataList
-        }
-    }
-
-    /** Creates the dialog UI and binds the data to the layout. */
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val doc =
-            arguments?.let {
-                BundleCompat.getParcelable(it, Shared.EXTRA_DOC, DocumentInfo::class.java)
-            }
-                ?: savedInstanceState?.let {
-                    BundleCompat.getParcelable(it, Shared.EXTRA_DOC, DocumentInfo::class.java)
-                }
-
-        if (doc == null) {
-            Log.e(TAG, "Document missing. Closing dialog.")
-            return super.onCreateDialog(savedInstanceState).apply { dismiss() }
-        }
-
-        val context = requireContext()
-        val customView = LayoutInflater.from(context).inflate(R.layout.get_info_m3, null)
-        val contentLayout: LinearLayout = customView.findViewById(R.id.content_layout)
-
-        val fileTypeLookup: Lookup<String, String> =
-            DocumentsApplication.getFileTypeLookup(getContext())
-
-        val dataList = createDataList(context, doc, fileTypeLookup)
-
-        dataList.forEachIndexed { position, item ->
-            val view =
-                when (item) {
-                    is ListItem.Header -> {
-                        LayoutInflater.from(context)
-                            .inflate(R.layout.get_info_header_m3, contentLayout, false)
-                            .apply { findViewById<TextView>(R.id.header_title).text = item.title }
-                    }
-
-                    is ListItem.Info -> {
-                        LayoutInflater.from(context)
-                            .inflate(R.layout.get_info_item_m3, contentLayout, false)
-                            .apply {
-                                findViewById<TextView>(R.id.item_label).text = item.label
-                                findViewById<TextView>(R.id.item_value).text = item.value
-
-                                // Check if the item is the first in the section.
-                                val isFirstInSection =
-                                    if (position == 0) {
-                                        true
-                                    } else {
-                                        dataList[position - 1] is ListItem.Header
-                                    }
-
-                                // Check if this is the last item in the section.
-                                val isLastInSection =
-                                    if (position == dataList.size - 1) {
-                                        true
-                                    } else {
-                                        dataList[position + 1] is ListItem.Header
-                                    }
-
-                                // Apply the correct background drawable based on the position of
-                                // the item in the section. The border radius is different based on
-                                // the position of the item.
-                                val backgroundRes =
-                                    when {
-                                        isFirstInSection && isLastInSection ->
-                                            R.drawable.get_info_item_single_m3
-
-                                        isFirstInSection -> R.drawable.get_info_item_top_m3
-                                        isLastInSection -> R.drawable.get_info_item_bottom_m3
-                                        else -> R.drawable.get_info_item_middle_m3
-                                    }
-
-                                setBackgroundResource(backgroundRes)
-
-                                // Apply the appropriate margins to the item according to the
-                                // position in the list.
-                                val params = layoutParams as ViewGroup.MarginLayoutParams
-                                val gapBetweenItems =
-                                    resources.getDimensionPixelSize(R.dimen.space_extra_small_1)
-                                if (isFirstInSection && isLastInSection) {
-                                    // No padding as it's the only item in the list.
-                                } else if (isFirstInSection) {
-                                    params.bottomMargin = gapBetweenItems
-                                } else if (isLastInSection) {
-                                    params.bottomMargin =
-                                        resources.getDimensionPixelSize(R.dimen.space_small_1)
-                                } else {
-                                    params.bottomMargin = gapBetweenItems
-                                }
-
-                                layoutParams = params
-                            }
-                    }
-                }
-            contentLayout.addView(view)
-        }
-
-        val closeButton: View = customView.findViewById(R.id.close_button)
-        closeButton.setOnClickListener { dismiss() }
-
-        val builder = MaterialAlertDialogBuilder(context).setView(customView)
-        return builder.create()
-    }
-
-    /** Resize the dialog to the requested size. */
-    override fun onStart() {
-        super.onStart()
-        (dialog as? AlertDialog)?.let { d ->
-            d.window?.let { w ->
-                val params = WindowManager.LayoutParams()
-                params.copyFrom(w.attributes)
-                val maxWidth = requireContext().resources.displayMetrics.widthPixels
-                // The window size is dialog size + right & left insets.
-                params.width = UnitUtils.dpToPx(requireContext(), WIDTH).coerceAtMost(maxWidth)
-                w.attributes = params
-            }
         }
     }
 }
